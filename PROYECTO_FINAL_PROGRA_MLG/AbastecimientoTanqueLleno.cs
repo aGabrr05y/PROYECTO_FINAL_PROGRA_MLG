fe03002be1aa390cd5b43795032d9cd421ceaf42
LizFer19 <lizadepaz@umes.edu.gt>
2026-06-02 15:00:28 -0600

    Agregue las clases hijas de abastecimiento para el tanque lleno y el prepago

---
+++ PROYECTO_FINAL_PROGRA_MLG/AbastecimientoTanqueLleno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROYECTO_FINAL_PROGRA_MLG
+{
+    internal class AbastecimientoTanqueLleno : Abastecimiento
+    {
+        public override void Procesar()
+        {
+            TotalCobrado = LitrosServidos * PrecioPorLitro;
+        }
+    }
+}
