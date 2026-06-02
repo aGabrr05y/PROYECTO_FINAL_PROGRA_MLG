3f3c8eb0bbe59ee1cc6d58fe4e8ba65bd9c2ead7
LizFer19 <lizadepaz@umes.edu.gt>
2026-06-02 14:52:46 -0600

    Agrugue la clase Bombas

---
+++ PROYECTO_FINAL_PROGRA_MLG/Bomba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROYECTO_FINAL_PROGRA_MLG
+{
+    internal class Bomba
+    {
+        public int Numero { get; set; }
+        public bool Ocupada { get; set; }
+        public int VecesUsada { get; set; }
+
+        public void Iniciar()
+        {
+            Ocupada = true;
+            VecesUsada++;
+        }
+
+        public void Finalizar()
+        {
+            Ocupada = false;
+        }
+    }
+}
