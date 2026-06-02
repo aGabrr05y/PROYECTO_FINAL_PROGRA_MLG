235264d2f583655d6678308468cad26fe5fb6947
Antonio Ramirez <antonioramirez@umes.edu.gt>
2026-06-02 12:34:04 -0600

    Se agregaron Botones

--- PROYECTO_FINAL_PROGRA_MLG/Form1.Designer.cs
+++ PROYECTO_FINAL_PROGRA_MLG/Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(266, 152);
+            button1.Location = new Point(84, 361);
             button1.Name = "button1";
             button1.Size = new Size(97, 48);
             button1.TabIndex = 0;
@@ -41,11 +42,21 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Location = new Point(386, 375);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 47);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -55,5 +66,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
