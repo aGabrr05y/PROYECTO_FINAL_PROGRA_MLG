88cad3bd4b6c4861fa6f01b847ce60029ce547bb
LizFer19 <lizadepaz@umes.edu.gt>
2026-06-02 14:51:22 -0600

    Agregue las clase Cliente con Getters y Setters

---
+++ PROYECTO_FINAL_PROGRA_MLG/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROYECTO_FINAL_PROGRA_MLG
+{
+    internal class Cliente
+    {
+        public string Nombre { get; set; }
+        public string NIT { get; set; }
+        public bool ConsumidorFinal { get; set; }
+    }
+}
