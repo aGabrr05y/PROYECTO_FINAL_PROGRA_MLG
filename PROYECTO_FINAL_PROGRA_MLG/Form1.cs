996db05a08aa0c31cc7a98dc0038345932f4522e
Moy2203 <moy2203@umes.edu.gt>
2026-06-03 01:19:36 -0600

    Cambie los parametros para el ID

--- PROYECTO_FINAL_PROGRA_MLG/Form1.cs
+++ PROYECTO_FINAL_PROGRA_MLG/Form1.cs
@@ -197,6 +197,7 @@ namespace PROYECTO_FINAL_PROGRA_MLG
                 // Asignar Id del abastecimiento al NIT del cliente
 
 
+
                 abastecimientoActual.Id = cliente.NIT;
             }
             else if (rdbTanqueLleno.Checked)
