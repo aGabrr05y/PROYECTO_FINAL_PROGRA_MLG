73f023ea7d4053fdc092f2867ae16955544bf7fc
Antonio <antonioramirez@umes.edu.gt>
2026-06-03 14:27:27 -0600

    arduino 10

--- PROYECTO_FINAL_PROGRA_MLG/Form1.cs
+++ PROYECTO_FINAL_PROGRA_MLG/Form1.cs
@@ -173,8 +173,6 @@ namespace PROYECTO_FINAL_PROGRA_MLG
         }
        
 
-
-
         private void btnActualizarCliente_Click(object sender, EventArgs e)
         {
             string nit = txtNIT.Text;
